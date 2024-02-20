cb84b75f9cd1e47fdd9de8b0808b3c9d5aced6a0
Francisco Watson <francisco@watson.com.ar>
2024-02-20 16:59:11 -0300

    Add Files

---
+++ Real-Time-Weather-Monitoring-and-Reporting-Service/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Real_Time_Weather_Monitoring_and_Reporting_Service
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
