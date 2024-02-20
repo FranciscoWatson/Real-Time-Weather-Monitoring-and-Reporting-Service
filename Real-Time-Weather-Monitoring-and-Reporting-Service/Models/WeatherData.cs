f01951900c4f9dbb26bd8d642b7635e28149950b
Francisco Watson <francisco@watson.com.ar>
2024-02-20 17:40:02 -0300

    Add WeatherData Model

---
+++ Real-Time-Weather-Monitoring-and-Reporting-Service/Models/WeatherData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Time_Weather_Monitoring_and_Reporting_Service.Models
+{
+    public class WeatherData
+    {
+        public string Location { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Location: {Location}, Temperature: {Temperature}, Humidity: {Humidity}";
+        }
+
+    }
+
+    
+}
