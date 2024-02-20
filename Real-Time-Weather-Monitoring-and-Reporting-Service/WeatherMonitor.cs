c12fb72cb0101814c2274790971e02e49e3b9a54
Francisco Watson <francisco@watson.com.ar>
2024-02-20 17:48:31 -0300

    Add WeatherMonitor

---
+++ Real-Time-Weather-Monitoring-and-Reporting-Service/WeatherMonitor.cs
@@ -0,0 +1,15 @@
+﻿using Real_Time_Weather_Monitoring_and_Reporting_Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Time_Weather_Monitoring_and_Reporting_Service
+{
+    public class WeatherMonitor
+    {
+        public WeatherData WeatherData { get; set; }
+
+    }
+}
