5a29b1e15e8fc0d75112501bb53134f62b7a8181
Francisco Watson <francisco@watson.com.ar>
2024-02-21 10:22:46 -0300

    Add Simple Factory for Weahter Input Adapter

---
+++ Real-Time-Weather-Monitoring-and-Reporting-Service/Simple Factory/WeatherInputFactory.cs
@@ -0,0 +1,27 @@
+﻿using Real_Time_Weather_Monitoring_and_Reporting_Service.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Time_Weather_Monitoring_and_Reporting_Service.Simple_Factory
+{
+    public class WeatherInputFactory
+    {
+        public static IWeatherInputAdapter CreateAdapter(string weatherData)
+        {         
+            string dataFormat = weatherData.Trim().StartsWith("{") ? "JSON" : "XML";
+
+            switch (dataFormat.ToUpperInvariant())
+            {
+                case "JSON":
+                    return new JsonWeatherInputAdapter();
+                case "XML":
+                    return new XmlWeatherInputAdapter();
+                default:
+                    throw new InvalidOperationException($"No adapter found for data format: {dataFormat}");
+            }
+        }
+    }
+}
