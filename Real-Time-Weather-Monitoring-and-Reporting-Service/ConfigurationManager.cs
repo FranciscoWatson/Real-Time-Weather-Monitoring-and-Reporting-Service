7f888289793c35f3fdcbb31ab984f423e3fda29f
Francisco Watson <francisco@watson.com.ar>
2024-02-21 18:30:00 -0300

    Change behavior of Configuration Manager

--- Real-Time-Weather-Monitoring-and-Reporting-Service/ConfigurationManager.cs
+++ Real-Time-Weather-Monitoring-and-Reporting-Service/ConfigurationManager.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Real_Time_Weather_Monitoring_and_Reporting_Service.Bots;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,31 @@ namespace Real_Time_Weather_Monitoring_and_Reporting_Service
 {
     public class ConfigurationManager
     {
-        public Dictionary<string, dynamic> ReadConfiguration(string configurationPath)
+        public List<IWeatherBot> ReadConfiguration(string configurationPath)
         {
             string configurationJson = File.ReadAllText(configurationPath);
-            return JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(configurationJson);
+            var dic = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(configurationJson);
+
+            IWeatherBot rainBot = new RainBot(
+                Convert.ToSingle(dic["RainBot"]["humidityThreshold"]),
+                dic["RainBot"]["message"].ToString(),
+                Convert.ToBoolean(dic["RainBot"]["enabled"]));
+
+            IWeatherBot sunBot = new SunBot(
+                Convert.ToSingle(dic["SunBot"]["temperatureThreshold"]),
+                dic["SunBot"]["message"].ToString(),
+                Convert.ToBoolean(dic["SunBot"]["enabled"])
+            );
+
+            IWeatherBot snowBot = new SnowBot(
+                Convert.ToSingle(dic["SnowBot"]["temperatureThreshold"]),
+                dic["SnowBot"]["message"].ToString(),
+                Convert.ToBoolean(dic["SnowBot"]["enabled"])
+            );
+
+            List<IWeatherBot> weatherBots = [rainBot, sunBot, snowBot];
+
+            return weatherBots;
         }
     }
 }
